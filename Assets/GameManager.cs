571b66dd9715e1cf10feb23b0931a363cd04e874
carbonroast <zergling3@gmail.com>
2025-09-26 18:07:21 -0400

    added seperate control schemes

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -62,7 +62,6 @@ public class GameManager : MonoBehaviour
     {
         playerCount++;
         Debug.Log($"Player Count - {playerCount}."); 
-
     }
 
     public void OnPlayerLeft(PlayerInput playerInput)
