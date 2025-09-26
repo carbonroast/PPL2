e9be77e58fd8d471d56f93af59819386196185fb
carbonroast <zergling3@gmail.com>
2025-09-26 04:19:52 -0400

    Add Input system

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,23 +11,9 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Set up the Player
-        GameObject playerGO = new GameObject("Player");
-        GameObject p1 = Instantiate(playerPrefab,Vector2.zero, quaternion.identity);
-        GameObject p2 = Instantiate(playerPrefab, Vector2.right, quaternion.identity);
-        p1.transform.SetParent(playerGO.transform);
-        p2.transform.SetParent(playerGO.transform);
-        p1.GetComponent<PlayerTileSelector>().Position = ((0,0));
-        p2.GetComponent<PlayerTileSelector>().Position = ((1,0));
+        CreatePlayer("Player");
+        CreateGrid();
 
-        playerGO.AddComponent<PlayerController>();
-
-        //Set up the grid
-        GameObject gridGO = new GameObject("GridManager");
-        gridGO.AddComponent<GridManager>();
-        gridGO.GetComponent<GridManager>().tilePrefab = tilePrefab;
-        gridGO.GetComponent<GridManager>().gridWidth = gridWidth;
-        gridGO.GetComponent<GridManager>().gridHeight = gridHeight;
         
     }
 
@@ -35,5 +21,19 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void CreatePlayer(string name)
+    {
+        Instantiate(playerPrefab,Vector2.zero, Quaternion.identity);
+    }
+
+    void CreateGrid()
+    {
+        GameObject gridGO = new GameObject("GridManager");
+        gridGO.AddComponent<GridManager>();
+        gridGO.GetComponent<GridManager>().tilePrefab = tilePrefab;
+        gridGO.GetComponent<GridManager>().gridWidth = gridWidth;
+        gridGO.GetComponent<GridManager>().gridHeight = gridHeight;
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,13 +4,36 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     GameObject grid;
     GameObject selectorLeft;
     GameObject selectorRight;
+    PlayerInputActions playerInputActions;
+
     // Start is called before the first frame update
+    void Awake()
+    {
+        playerInputActions = new PlayerInputActions();        
+    }
+
+    void OnEnable()
+    {
+        playerInputActions.Player.Move.performed += Movement;
+        playerInputActions.Player.Move.Enable();
+
+        playerInputActions.Player.Swap.performed += Swap;
+        playerInputActions.Player.Swap.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerInputActions.Player.Move.Disable();
+        playerInputActions.Player.Swap.Disable();
+    }
+
     void Start()
     {
         grid = GameObject.Find("GridManager"); 
@@ -21,42 +44,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKeyDown)
-        {
-            Movement();
-            SwapTiles();
-        }
+    }
+
+    void Movement(InputAction.CallbackContext obj)
+    {
+        Debug.Log(obj);
+        // Vector2 movement = Vector2.zero;
+        // if (Input.GetKeyDown(KeyCode.W))
+        //     movement.y += 1;
+        // if (Input.GetKeyDown(KeyCode.S))
+        //     movement.y -= 1;
+        // if (Input.GetKeyDown(KeyCode.A))
+        //     movement.x -= 1;
+        // if (Input.GetKeyDown(KeyCode.D))
+        //     movement.x += 1;
+
+        // Vector2 left = new Vector2(selectorLeft.transform.position.x, selectorLeft.transform.position.y);
+        // Vector2 right = new Vector2(selectorRight.transform.position.x, selectorRight.transform.position.y);
+
+        // if(grid.GetComponent<GridManager>().ValidMovement(left + movement) && grid.GetComponent<GridManager>().ValidMovement(right + movement))
+        // {
+        //     selectorLeft.GetComponent<PlayerTileSelector>().Position = ((int)(left.x + movement.x), (int)(left.y + movement.y));
+        //     selectorRight.GetComponent<PlayerTileSelector>().Position = ((int)(right.x + movement.x), (int)(right.y + movement.y));
+        // }
 
     }
 
-    void Movement()
+    void Swap(InputAction.CallbackContext obj)
     {
-        Vector2 movement = Vector2.zero;
-        if (Input.GetKeyDown(KeyCode.W))
-            movement.y += 1;
-        if (Input.GetKeyDown(KeyCode.S))
-            movement.y -= 1;
-        if (Input.GetKeyDown(KeyCode.A))
-            movement.x -= 1;
-        if (Input.GetKeyDown(KeyCode.D))
-            movement.x += 1;
-
-        Vector2 left = new Vector2(selectorLeft.transform.position.x, selectorLeft.transform.position.y);
-        Vector2 right = new Vector2(selectorRight.transform.position.x, selectorRight.transform.position.y);
-
-        if(grid.GetComponent<GridManager>().ValidMovement(left + movement) && grid.GetComponent<GridManager>().ValidMovement(right + movement))
-        {
-            selectorLeft.GetComponent<PlayerTileSelector>().Position = ((int)(left.x + movement.x), (int)(left.y + movement.y));
-            selectorRight.GetComponent<PlayerTileSelector>().Position = ((int)(right.x + movement.x), (int)(right.y + movement.y));
-        }
-
-    }
-
-    void SwapTiles()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            grid.GetComponent<GridManager>().SwapTiles(selectorLeft, selectorRight);
-        }
+        Debug.Log(obj);
+        // if(Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     grid.GetComponent<GridManager>().SwapTiles(selectorLeft, selectorRight);
+        // }
     }
 }
